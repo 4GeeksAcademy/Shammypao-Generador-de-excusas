9cd55ccac333ca6493ccca7431891a58c147cedf
Shammy Murillo <75450484+sammypao@users.noreply.github.com>
2025-11-24 22:16:33 +0000

    Generador terminado

--- app.js
+++ app.js
@@ -1,27 +1,30 @@
-function generateExcuse() {
-    const who = ['El perro', 'Mi abuela', 'Su tortuga', 'Mi pájaro'];
-    const action = ['se comió', 'orino en', 'aplastó', 'rompió'];
-    const what = ['mi tarea', 'las llaves', 'el coche', 'el celular'];
-    const when = ['antes de la clase', 'justo a tiempo', 'durante mi almuerzo',
-        'cuando terminé', 'mientras rezaba'];
-
-    const whoInd = Math.floor(Math.random() * who.length);
-    const actionInd = Math.floor(Math.random() * action.length);
-    const whatInd = Math.floor(Math.random() * what.length);
-    const whenInd = Math.floor(Math.random() * when.length);
+const who = ['El perro', 'Mi abuela', 'Su tortuga', 'Mi pájaro'];
+const action = ['se comió', 'orino en', 'aplastó', 'rompió'];
+const what = ['mi tarea', 'las llaves', 'el coche', 'el celular'];
+const when = ['antes de la clase', 'justo a tiempo', 'durante mi almuerzo', 'cuando terminé', 'mientras rezaba'];
 
 
-    const excuse = `${who[whoInd]} ${action[actionInd]} ${whatInd} ${whenInd}.`;
+const rand = (arr) => arr[Math.floor(Math.random() * arr.length)];
 
+const generateExcuse = () => `${rand(who)} ${rand(action)} ${rand(what)} ${rand(when)}.`;
+
+const renderExcuse = () => {
     const excuseElement = document.getElementById("excuse");
     if (excuseElement) {
-        excuseElement.innerHTML = excuse;
+
+        excuseElement.innerHTML = generateExcuse();
     }
 };
 
+
 window.onload = () => {
-   const generateBtn = document.getElementById("generate");
+    
+    renderExcuse();
+
+    
+    const generateBtn = document.getElementById("generate");
     if (generateBtn) {  
-        generateBtn.addEventListener("click", generateExcuse);
-    };
+        // Al hacer click, llamamos a la función de renderizado
+        generateBtn.addEventListener("click", renderExcuse);
+    }
 };
